5514ef446e4afee52caa222c4b179f7b010379be
david gray <david@creativecodestudios.com>
2019-01-20 13:14:59 +0000

    basic routes, logging and set ups

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const app = express();
-const bodyParser = require()
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const recordRoutes = require('./api/routes/records');
+
+app.use(morgan('dev'));
+app.use(bodyParser).urlencoded({extended: false});
+app.use(bodyParser.json);
+
+app.use('/records', recordRoutes);
 
 app.use((req, res, next) => {
-    res.status(200).json({
-        message: 'Server is running on port 3000'
-    })
+    const error = new Error('Not Found');
+    res.status = 404;
+    next(error);
 });
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({error: {
+        message: error.message
+    }})
+});
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const http = require('http');
+const app = require('./app')
 
-const port = process.evn.PORT || 3000;
+const port = 3000;
 
-const server = http.createServer();
+const server = http.createServer(app);
 
 server.listen(port);
 
